a4767950cd483ef0d86fdd20ac1aa3b59bcd787c
Anwesh43 <anweshthecool0@gmail.com>
2018-11-01 13:59:59 +0530

    adding LDSNode class

--- LineDisappearingStage.ts
+++ LineDisappearingStage.ts
@@ -1,6 +1,10 @@
 const w : number = window.innerWidth, h : number = window.innerHeight
 const nodes : number = 5
 const k : number = 4
+
+const divideScale = (scale : number, j : number) : number => {
+    return Math.min(0.5, Math.max(0, scale - j * 0.5)) * 2
+}
 class LineDisappearingStage {
     canvas : HTMLCanvasElement = document.createElement('canvas')
     context : CanvasRenderingContext2D
@@ -69,5 +73,68 @@ class Animator {
             this.animated = false
             clearInterval(this.interval)
         }
+    }
+}
+
+class LDSNode {
+    next : LDSNode
+    prev : LDSNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new LDSNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+
+    draw(context : CanvasRenderingContext2D) {
+        const gap : number = w / (nodes + 1)
+        const size : number = (gap) / 3
+        context.save()
+        context.translate(gap * (1 + this.i), h / 2)
+        for (var j = 0; j < 2; j++) {
+            const sc : number = divideScale(this.state.scale, j)
+            const sc1 : number = divideScale(sc, 0)
+            const sc2 : number = divideScale(sc, 1)
+            const sf : number = 1 - 2 * j
+            context.save()
+            context.translate(-size, h/2 * sf * sc2)
+            context.rotate(Math.PI/2 * sc1 * sf)
+            context.beginPath()
+            context.moveTo(0, 0)
+            context.lineTo(0, 2 * size)
+            context.stroke()
+            context.restore()
+        }
+        context.restore()
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LDSNode {
+        var curr : LDSNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this
     }
 }
